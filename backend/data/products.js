ca6300337fb50a1de55dbd6283c2a34f186c1acc
SAT1ORU <pastranabryan2@gmail.com>
2025-06-02 04:13:33 -0500

    Update products.js

--- backend/data/products.js
+++ backend/data/products.js
@@ -4,70 +4,80 @@ const products = [
     name: "Auriculares Gamer RGB HyperX",
     price: 149000,
     image: "/img/auriculares.jpg",
-    brand: "HyperX" 
+    brand: "HyperX",
+    category: "Auriculares"
   },
   {
     id: 2,
     name: "Teclado Mecánico RGB Corsair K95",
     price: 189000,
     image: "/img/teclado.jpg",
-    brand: "Corsair"
+    brand: "Corsair",
+    category: "Teclados"
   },
   {
     id: 3,
     name: "Mouse Gamer 7200DPI Logitech G502",
     price: 99000,
     image: "/img/mouse.jpg",
-    brand: "Logitech" 
+    brand: "Logitech",
+    category: "Mouses"
   },
   {
     id: 4,
     name: "Monitor Curvo 27' Samsung Odyssey G7",
     price: 1800000,
     image: "/img/monitor.jpg",
-    brand: "Samsung"
+    brand: "Samsung",
+    category: "Monitores"
   },
   {
     id: 5,
     name: "Webcam Logitech C920 HD",
     price: 350000,
     image: "/img/webcam.jpg",
-    brand: "Logitech"
+    brand: "Logitech",
+    category: "Webcams"
   },
   {
     id: 6,
     name: "Disco Duro Externo 1TB Seagate",
     price: 280000,
     image: "/img/disco_duro.jpg",
-    brand: "Seagate"
+    brand: "Seagate",
+    category: "Almacenamiento"
   },
   {
     id: 7,
     name: "Router Wi-Fi 6 TP-Link AX1500",
     price: 420000,
     image: "/img/router.jpg",
-    brand: "TP-Link"
+    brand: "TP-Link",
+    category: "Redes"
   },
   {
     id: 8,
     name: "Tarjeta Gráfica NVIDIA RTX 4070",
     price: 4500000,
     image: "/img/gpu.jpg",
-    brand: "NVIDIA" 
+    brand: "NVIDIA",
+    category: "Tarjetas Gráficas" 
   },
   {
     id: 9,
     name: "Silla Gamer Ergonómica",
     price: 750000,
     image: "/img/silla_gamer.jpg",
-    brand: "ErgoChair" 
+    brand: "ErgoChair",
+    category: "Sillas Gamer"
   },
   {
     id: 10,
     name: "Micrófono USB Blue Yeti",
     price: 520000,
     image: "/img/microfono.jpg",
-    brand: "Blue"
+    brand: "Blue",
+    category: "Micrófonos"
   }
 ];
 
